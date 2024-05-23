b05567529351ee9ccebb815588c30ab368d00d11
Navin Kumaran <navinkumaran2004@gmail.com>
2024-05-23 14:19:29 +0530

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data'
+
+const Footer = () => {
+  return (
+    <footer className="w-full pb-10 mb-[70px] md:mb-5" id="contact">
+        <div className='flex flex-col items-center'>
+            <h1 className='heading lg:max-w-[45vw]'>Ready to take {' '}<span className='text-purple'>your</span>{' '}outside digital presence to the next level</h1>
+            <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss hoe i can help you acheive your goals</p>
+            <a href='mailto:navinkumaran2004@gmail.com'>
+                <MagicButton 
+                title="Let's get in touch"
+                icon={<FaLocationArrow />}
+                position='right'
+                />
+            </a>
+        </div>
+        <div className='flex mt-16 md:flex-row flex-col lg:px-40  justify-between items-center'>
+            <p className='md:text-base text-small md:font-normal font-thin'>Copyright ©️ 2024 Navin Kumaran</p>
+            <div className='flex items-center md:gap-3 mt-3 gap-6'>
+                {socialMedia.map((profile)=>(
+                    <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
+                        <img 
+                        src={profile.img}
+                        alt={profile.alt}
+                        height={20} width={20}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ data/index.ts
@@ -0,0 +1,236 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building CGPA Calculator For VCET",
+    description: "CGPA Calculator",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "CGPA Calculator VCET",
+    des: "The CGPA calculator developed with the MERN Stack, simplifies academic tracking with mobile optimization and MongoDB for efficient data handling.",
+    img: "/cgpa.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://cgpa-calculator-l1l7.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "MERN Stack Blog Website",
+    des: "A Blog Website Designed Usign MERN Stack Check out the website and describe your experience of using it.",
+    img: "/urbanuplift.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://urban-uplift.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "NIKE Design Site",
+    des: "A Visually appealing UI/UX Design made using Tailwind CSS",
+    img: "/nike.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://lnkd.in/g2WypRD2",
+  },
+  {
+    id: 4,
+    title: "Weather Forecasting Website",
+    des: "Weather Forcasting for a full week which also includes the climatic change graph with the hep of openweather API",
+    img: "/weather.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://navin2k4.github.io/Weather-Website/",
+  },
+  {
+    id: 5,
+    title: "ChatVibe",
+    des: "An online chatting application made using Django",
+    img: "/chatvibe.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/Navin2k4/ChatVibe",
+  },
+  {
+    id: 6,
+    title: "APJ Abdun Kalam JEE/NEET",
+    des: "Designed and developed a Online education portal landing page with pure html and css",
+    img: "/apj.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://navin2k4.github.io/APJ-NEET-JEE/#SERVICE",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Development Intern @ Teachnook",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Development Intern @ Exposys Data Labs",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Android Application Developer @ NSIC Chennai",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    alt:'Git',
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    alt:'Twitter',
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    alt:'Linkedin',
+    img: "/link.svg",
+  },
+];
