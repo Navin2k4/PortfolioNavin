36206faae83fddf18448d9d03deb9ea38eaa7c6e
Navin Kumaran <navinkumaran2004@gmail.com>
2024-05-29 13:22:26 +0530

    Added Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,7 +28,7 @@ export default function RootLayout({
           >
             {children}
           <Analytics />
-
+          <SpeedInsights />
           </ThemeProvider>
         </body>
     </html>
